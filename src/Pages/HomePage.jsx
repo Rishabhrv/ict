74d01708b34cbdb7f8d5ea9ff068f09463d903c5
Rishabh <rishabhvyas472@@gmail.com>
2025-11-18 11:43:03 +0530

    access update

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -41,6 +41,7 @@ const VALID_ACCESS = {
   "Tasks": "tasks",
   "Details": "details",
   "Message": "messages",
+  "Attendance": "attendance",
   // Non-loop buttons
   "Add Book": "add_book_dialog",
   "Authors Edit": "edit_author_detail"
