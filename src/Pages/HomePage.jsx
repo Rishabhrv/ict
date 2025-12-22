8e36306355c64904ea582cc7d34f954a4ed1a5e5
Rishabh <rishabhvyas472@@gmail.com>
2025-12-22 10:00:22 +0530

    button access update

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -25,26 +25,26 @@ const VALID_APPS = {
 
 const VALID_ACCESS = {
   // Loop buttons (table)
-  "ISBN": "manage_isbn_dialog",
-  "Payment": "manage_price_dialog",
-  "Authors": "edit_author_dialog",
-  "Operations": "edit_operation_dialog",
-  "Printing & Delivery": "edit_inventory_delivery_dialog",
-  "DatadashBoard": "datadashoard",
-  "Advance Search": "advance_search",
-  "Team Dashboard": "team_dashboard",
-  "Print Management": "print_management",
-  "Inventory": "inventory",
-  "Open Author Positions": "open_author_positions",
-  "Pending Work": "pending_books",
-  "IJISEM": "ijisem",
-  "Tasks": "tasks",
-  "Details": "details",
-  "Message": "messages",
-  "Attendance" : "attendance",
-  // Non-loop buttons
-  "Add Book": "add_book_dialog",
-  "Authors Edit": "edit_author_detail"
+   "ISBN": "manage_isbn_dialog",
+    "Payment": "manage_price_dialog",
+    "Authors": "edit_author_dialog",
+    "Operations": "edit_operation_dialog",
+    "Printing & Delivery": "edit_inventory_delivery_dialog",
+    "DatadashBoard": "datadashoard",
+    "Advance Search": "advance_search",
+    "Team Dashboard": "team_dashboard",
+    "Print Management": "print_management",
+    "Inventory": "inventory",
+    "Open Author Positions": "open_author_positions",
+    "Pending Work": "pending_books",
+    "IJISEM": "ijisem",
+    "Tasks": "tasks",
+    "Details": "details",
+    "Message": "messages",
+    "Attendance": "attendance",
+    "Extra Books": "extra_books",
+    "Add Book": "add_book_dialog",
+    "Authors Edit": "edit_author_detail"
 };
 
 
